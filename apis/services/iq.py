5f6a0148f8436bb183b8f1098cbf08acd5a77cd2
Daniel Suniaga <105082657+danielsuniaga@users.noreply.github.com>
2024-06-08 10:50:45 -0500

    Update iq.py

--- apis/services/iq.py
+++ apis/services/iq.py
@@ -237,14 +237,6 @@ class cases_iq:
 
         candles = self.removed_candle_close(candles,self.candle_removed)
 
-        candles[0]=1.05
-
-        candles[1]=1.04
-
-        candles[2]=1.03
-
-        candles[3]=1.02
-
         if all(candles[i] < candles[i+1] for i in range(self.candle_analized - 1)):
 
             self.set_type(self.type_entry_long)
