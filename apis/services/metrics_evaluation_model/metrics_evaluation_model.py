374fa8a6708cc3706907333fef281ea275f7200b
Daniel Suniaga <danieldsuniaga@gmail.com>
2024-11-15 19:55:26 -0500

    add metrics in telegram notification

--- apis/services/metrics_evaluation_model/metrics_evaluation_model.py
+++ apis/services/metrics_evaluation_model/metrics_evaluation_model.py
@@ -9,6 +9,14 @@ class case_metrics_evaluation_model(icase_metrics_evaluation_model):
 
     metrics_evaluation_model = None
 
+    accuracy = None
+
+    precision = None
+
+    recall = None
+
+    f1 = None
+
     def __init__(self,cursor):
        
        self.metrics_evaluation_model = repositories_metrics_evaluation_model.repositories_metrics_evaluation_model(cursor)
@@ -16,6 +24,46 @@ class case_metrics_evaluation_model(icase_metrics_evaluation_model):
        self.init_models()
 
        self.init_state_default()
+
+    def get_accuracy(self):
+
+        return self.accuracy
+    
+    def set_accuracy(self,value):
+
+        self.accuracy = value
+
+        return True
+    
+    def get_precision(self):
+
+        return self.precision
+    
+    def set_precision(self,value):
+
+        self.precision = value
+
+        return True
+    
+    def get_recall(self):
+
+        return self.recall 
+    
+    def set_recall(self,value):
+
+        self.recall = value
+
+        return True
+    
+    def get_f1(self):
+
+        return self.f1 
+    
+    def set_f1(self,value):
+
+        self.f1 = value
+
+        return True
 
     def init_state_default(self):
 
@@ -42,6 +90,14 @@ class case_metrics_evaluation_model(icase_metrics_evaluation_model):
         return self.models['general_logistic']
 
     def add(self,data):
+
+        self.set_accuracy(data['accuracy'])
+
+        self.set_precision(data['precision'])
+
+        self.set_f1(data['f1'])
+
+        self.set_recall(data['recall'])
 
         result = self.metrics_evaluation_model.add(data)
 
